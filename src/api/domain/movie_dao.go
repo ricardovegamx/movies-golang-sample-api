210c8ae55a5afeaf463a365df2d5c7b5f3a953ad
Ricardo Vega <dev.ricardo.vega@gmail.com>
2020-09-15 18:01:31 -0500

    added mongo to docker compose

--- src/api/domain/movie_dao.go
+++ src/api/domain/movie_dao.go
@@ -1,7 +1,12 @@
 package domain
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var MovieDao MovieDaoInterface
@@ -18,6 +23,22 @@ type movieDao struct{}
 
 func init() {
 	MovieDao = &movieDao{}
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }
 
 func (m *movieDao) GetAll() *[]Movie {
